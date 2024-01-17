576857b8d44b4d5aec24418730b520e4b7cc619d
Foysal Hossain <foysalhossain772@gmail.com>
2024-01-17 17:13:18 +0600

    email subscribe section added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -86,6 +86,10 @@ const Products = () => {
                             ))
                         }
                     </div>
+                    {/* view all button */}
+                    <div className="flex justify-center">
+                        <button className="text-center mt-10 cursor-pointer bg-primary text-white py-1 px-6 rounded-md">View All</button>
+                    </div>
                 </div>
             </div>
         </div>
