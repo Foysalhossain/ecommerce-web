0ad5bc940e083049b50c8110a5d6a234452932cb
Foysal Hossain <foysalhossain772@gmail.com>
2024-01-17 17:19:42 +0600

    testimonial header section added

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,20 @@
+
+
+const Testimonials = () => {
+    return (
+        <div className="py-10">
+            <div className="container">
+                {/* Header section */}
+                <div className="text-center mb-10 max-w-[600px] mx-auto">
+                    <p data-aos="fade-up" className="text-sm text-primary">What our customers are saying</p>
+                    <h1 data-aos="fade-up" className="text-3xl font-bold py-2">Testimonials</h1>
+                    <p data-aos="fade-up" className="text-xs text-gray-400">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum alias nihil corrupti ratione cupiditate explicabo placeat natus? Aperiam, deserunt unde!
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonials;
